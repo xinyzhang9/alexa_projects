b7cfc411a43cb9d7ab3164b83bee8c5c91fac3ae
Xinyang <xinyzhang9@gmail.com>
2016-06-10 18:24:45 -0700

    squad helper for portugal

---
+++ june/src/index.js
@@ -0,0 +1,116 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
+ * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
+ *
+ * - Custom slot type: demonstrates using custom slot types to handle a finite set of known values
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, ask Euro2016 Helper how to make paper."
+ *  Alexa: "(reads back recipe for paper)"
+ */
+
+'use strict';
+
+var AlexaSkill = require('./AlexaSkill'),
+    recipes = require('./recipes');
+
+var APP_ID = undefined; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+/**
+ * Euro2016Helper is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var Euro2016Helper = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+Euro2016Helper.prototype = Object.create(AlexaSkill.prototype);
+Euro2016Helper.prototype.constructor = Euro2016Helper;
+
+Euro2016Helper.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    var speechText = "Welcome to the clash royale Helper. You can ask a question like, can you tell me something about Cristiano Ronaldo? ... Now, what can I help you with.";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "For instructions on what you can say, please say help me.";
+    response.ask(speechText, repromptText);
+};
+
+Euro2016Helper.prototype.intentHandlers = {
+    "SquadIntent": function (intent, session, response) {
+        var itemSlot = intent.slots.Item,
+            itemName;
+        if (itemSlot && itemSlot.value){
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        var cardTitle = "description for " + itemName,
+            recipe = recipes[itemName],
+            speechOutput,
+            repromptOutput;
+        if (recipe) {
+            speechOutput = {
+                speech: recipe,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.tellWithCard(speechOutput, cardTitle, recipe);
+        } else {
+            var speech;
+            if (itemName) {
+                speech = "I'm sorry, I currently do not know the description for " + itemName + ". What else can I help with?";
+            } else {
+                speech = "I'm sorry, I currently do not know that description. What else can I help with?";
+            }
+            speechOutput = {
+                speech: speech,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            repromptOutput = {
+                speech: "What else can I help with?",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.ask(speechOutput, repromptOutput);
+        }
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "You can ask questions about the Portugal Sqad in EURO 2016 such as, can you tell me something about Cristiano Ronaldo, or, you can say exit... Now, what can I help you with?";
+        var repromptText = "You can say things like, can you tell me something about Cristiano Ronaldo, or you can say exit... Now, what can I help you with?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+};
+
+exports.handler = function (event, context) {
+    var euro2016Helper = new Euro2016Helper();
+    euro2016Helper.execute(event, context);
+};
